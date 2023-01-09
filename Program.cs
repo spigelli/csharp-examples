befd5bfbe3bbbcb8e8294eaa0167d2a3e9b1e5b3
Isaac Spiegel <isaacspiegel@gmail.com>
2023-01-09 15:17:39 -0600

    Add some variables examples

--- Program.cs
+++ Program.cs
@@ -10,3 +10,37 @@ namespace HelloWorld
     }
   }
 }
+
+namespace Variables {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("===W3 Schools Variables===");
+
+      Console.WriteLine("Name Example:");
+      string name = "Isaac Spiegel";
+      Console.WriteLine(name);
+
+      Console.WriteLine("Int Example:");
+      int age = 22;
+      Console.WriteLine(age);
+
+      Console.WriteLine("Int Declare-First Example:");
+      int numberOfCatsOwned;
+      numberOfCatsOwned = 2;
+      Console.WriteLine(numberOfCatsOwned);
+
+      Console.WriteLine("Int Re-Assign Example:");
+      int numberOfDogsOwned = 2;
+      numberOfDogsOwned = 0;
+      Console.WriteLine(numberOfDogsOwned);
+
+      Console.WriteLine("Other Primitive Types Example:");
+      double myDoubleNum = 5.99D;
+      char myLetter = 'D';
+      bool correct = true;
+      Console.WriteLine(myDoubleNum);
+      Console.WriteLine(myLetter);
+      Console.WriteLine(correct);
+    }
+  }
+}
